5cb3ef4b11cae61ac16b3eb0a399d0ac70dfc554
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2022-08-15 23:39:20 +0530

    Update _app.js

--- Project1/pages/_app.js
+++ Project1/pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import NavBar from './components/NavBar'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <NavBar/>
+   <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
